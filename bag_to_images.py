3ce1b70c23961375e9b57d2d65c387983b566b4a
ChrisSun99 <mengtisun@berkeley.edu>
2022-07-30 14:32:04 -0400

    Init commit

---
+++ bag_to_images.py
@@ -0,0 +1,81 @@
+"""Extract images and poses from a rosbag.
+"""
+
+import os
+import argparse
+import cv2
+import rosbag
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+def bag_to_images():
+    """Extract a folder of images from a rosbag.
+    Command to run: python3 bag_to_images.py raw_10.bag /data /camera/depth/image_rect_raw
+    """
+    parser = argparse.ArgumentParser(description="Extract images from a ROS bag.")
+    parser.add_argument("bag_file", help="Input ROS bag.")
+    parser.add_argument("output_dir", help="Output directory.")
+    parser.add_argument("image_topic", help="Image topic.")
+    args = parser.parse_args()
+
+    print("Extract images from %s on topic %s into %s" % (args.bag_file,
+                                                          args.image_topic, args.output_dir))
+
+    bag = rosbag.Bag(args.bag_file, "r")
+    bridge = CvBridge()
+    count = 0
+    for topic, msg, t in bag.read_messages(topics=[args.image_topic]):
+        cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+
+        cv2.imwrite(os.path.join(args.output_dir, "frame%06i.png" % count), cv_img)
+        print("Wrote image %i" % count)
+
+        count += 1
+        # Rosbag is too large, we only keep the first 10 images
+        if count == 10:
+            break 
+
+    bag.close()
+
+    return
+
+
+def bag_to_pose(bagfile, pose_topic, out_filename):
+    """
+    Write /joint_states to a file
+    """
+    n = 0
+    f = open(out_filename, 'w')
+    f.write('# timestamp tx ty tz qx qy qz qw\n')
+    with rosbag.Bag(bagfile, 'r') as bag:
+        for (topic, msg, ts) in bag.read_messages(topics=str(pose_topic)):
+            f.write('%.12f \n position: %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f \n \
+            velocity: %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f \n' %
+                    (msg.header.stamp.to_sec(),
+                    msg.position[0], 
+                    msg.position[1],
+                    msg.position[2],
+                    msg.position[3],
+                    msg.position[4],
+                    msg.position[5],
+                    msg.position[6],
+                    msg.position[7],
+                    msg.position[8],
+                    msg.velocity[0],
+                    msg.velocity[1],
+                    msg.velocity[2],
+                    msg.velocity[3],
+                    msg.velocity[4],
+                    msg.velocity[5],
+                    msg.velocity[6],
+                    msg.velocity[7],
+                    msg.velocity[8]))
+           
+            n += 1
+            if n == 10:
+                break
+    print('wrote ' + str(n) + ' imu messages to the file: ' + out_filename) 
+
+if __name__ == '__main__':
+    # bag_to_images()
+    bag_to_pose("raw_10.bag", "/joint_states", "./data/joint_states.txt")
---
+++ urdf_filter.py
@@ -0,0 +1,190 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from pydrake.multibody.parsing import Parser
+from pydrake.multibody.plant import MultibodyPlant, AddMultibodyPlantSceneGraph
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import DiagramBuilder
+from pydrake.systems.rendering import MultibodyPositionToGeometryPose
+from pydrake.systems.primitives import TrajectorySource
+from pydrake.math import RigidTransform
+from pydrake.geometry import SceneGraph, DrakeVisualizer
+from pydrake.trajectories import PiecewisePolynomial
+from pydrake.all import (AbstractValue, AddMultibodyPlantSceneGraph, AngleAxis,
+                         BaseField, ConstantValueSource, CsdpSolver,
+                         DepthImageToPointCloud, DiagramBuilder,
+                         DifferentialInverseKinematicsIntegrator,
+                         DifferentialInverseKinematicsParameters, EventStatus, LeafSystem,
+                         MakePhongIllustrationProperties, MathematicalProgram,
+                         MeshcatPointCloudVisualizer, MeshcatVisualizer,
+                         MeshcatVisualizerParams, Parser, PiecewisePolynomial,
+                         PiecewisePose, PointCloud, RigidTransform,
+                         RollPitchYaw, RotationMatrix, Simulator, StartMeshcat,
+                         ge)
+from pydrake.common import FindResourceOrThrow    
+from pydrake.multibody.meshcat import JointSliders    
+from manipulation.meshcat_cpp_utils import (AddMeshcatTriad,
+                                            draw_open3d_point_cloud)
+from manipulation.mustard_depth_camera_example import MustardPointCloud
+from manipulation.open3d_utils import create_open3d_point_cloud
+from manipulation.scenarios import (AddMultibodyTriad, AddRgbdSensor,
+                                    MakeManipulationStation)
+from manipulation.utils import AddPackagePaths, FindResource
+import numpy as np
+import os
+import rospy 
+from sensor_msgs.msg import JointState
+
+"""
+Note: I install drake from source through CMake.
+DONE: Use JointState to simulate robot states.
+TODO: For each frame, generate a depth image; subtract the depth image from the original depth image
+"""
+
+class FrankaPlaybackSim:
+    def __init__(self, meshcat, position, velocity):
+        self.meshcat = meshcat
+        self.builder = DiagramBuilder()
+        
+        # Add a cube as MultibodyPlant
+        self.plant, self.scene_graph = AddMultibodyPlantSceneGraph(self.builder, time_step=0.0)
+        
+        # self.X_model = RigidTransform(RollPitchYaw(-np.pi/2., 0, -np.pi/2.), [0, 0, 0.09515])
+        self.X_model = RigidTransform.Identity()
+        
+        self.parser = Parser(self.plant)
+        self.model_file = FindResourceOrThrow("drake/manipulation/models/franka_description/urdf/panda_arm_hand.urdf")
+        self.model = self.parser.AddModelFromFile(
+            self.model_file
+        )
+        
+        self.plant.WeldFrames(self.plant.world_frame(),
+                            self.plant.GetFrameByName("panda_link0", self.model),
+                            self.X_model)
+
+        # Add a box for the camera in the environment.
+        self.X_Camera = RigidTransform(
+            RollPitchYaw(0, -0.2, 0.2).ToRotationMatrix().multiply(
+                RollPitchYaw(-np.pi/2.0, 0, np.pi/2.0).ToRotationMatrix()),
+            [.5, .1, .2])
+        self.camera_instance = self.parser.AddModelFromFile(FindResource("models/camera_box.sdf"))
+        self.camera_frame = self.plant.GetFrameByName("base", self.camera_instance)    
+        self.plant.WeldFrames(self.plant.world_frame(), self.camera_frame, self.X_Camera)
+        AddMultibodyTriad(self.camera_frame, self.scene_graph, length=.1, radius=0.005)
+        self.plant.Finalize()
+
+        # Visualize in meshcat
+        self.params = MeshcatVisualizerParams()
+        self.visualizer = MeshcatVisualizer.AddToBuilder(
+            self.builder, self.scene_graph, self.meshcat, self.params
+        )
+        self.camera = AddRgbdSensor(self.builder, self.scene_graph, X_PC=RigidTransform(),
+                           parent_frame_id=self.plant.GetBodyFrameIdOrThrow(
+                               self.camera_frame.body().index()))
+        self.camera.set_name("rgbd_sensor")
+
+        # Export the camera outputs
+        # self.builder.ExportOutput(self.camera.color_image_output_port(), "color_image")
+        # self.builder.ExportOutput(self.camera.depth_image_32F_output_port(), "depth_image")
+        
+
+        # Setup contexts
+        # TODO: Simulation in server is not rendering
+        # self.context = self.diagram.CreateDefaultContext()
+        # self.diagram.Publish(self.context)
+        # self.plant_context = self.plant.GetMyMutableContextFromRoot(self.context)
+        # self.plant.SetPositions(self.plant_context, position)
+        # self.plant.get_actuation_input_port().FixValue(self.plant_context, np.zeros(9))
+        # self.simulator = Simulator(self.diagram, self.context)
+        # self.simulator.set_target_realtime_rate(1.0)
+        # meshcat.start_recording()
+        # self.simulator.AdvanceTo(0.1)
+        # meshcat.stop_recording()
+        # meshcat.publish_recording()
+
+
+        # ROS 
+        self.diagram = self.build()
+        self.simulator = Simulator(self.diagram)
+        self.simulator.set_target_realtime_rate(1.0)
+        self.simulator.set_publish_every_time_step(False)
+        self.context = self.simulator.get_mutable_context()
+        self.state = self.context.get_mutable_continuous_state_vector()
+        self.state.SetFromVector(np.zeros(9*2))
+        self.simulator.Initialize()
+
+        # Real depth images extracted from rosbag
+        self.real_depth_img = mpimg.imread('./data/frame000000.png')
+
+    def build(self):
+        # Setup JointSlider to control joint configuration
+        # default_interactive_timeout = 1.0 if "TEST_SRCDIR" in os.environ else None
+        # sliders = self.builder.AddSystem(JointSliders(meshcat, self.plant))
+        diagram = self.builder.Build()
+        # sliders.Run(diagram, default_interactive_timeout)
+        diagram.set_name("depth_camera_demo_system")
+        return diagram
+
+    
+    def visualize(self):
+        def callback(msg):
+            global q, v, f
+            q = np.array(msg.position)
+            v = np.array(msg.velocity)
+            f = np.array(msg.effort)
+
+        rospy.init_node('listener', anonymous=True)
+        
+        rospy.Subscriber('/joint_states', JointState, callback=callback)
+        rate = rospy.Rate(4)
+        rate.sleep()
+        while not rospy.is_shutdown():
+            
+            self.state = self.context.get_mutable_continuous_state_vector()
+            self.state.SetFromVector(np.append(q,v))
+            # TODO: Should I place the camera export here?
+            self.builder.ExportOutput(self.camera.color_image_output_port(), "color_image")
+            self.builder.ExportOutput(self.camera.depth_image_32F_output_port(), "depth_image")
+            self.plot_camera_images()
+            self.simulator.Initialize()
+            # self.builder.ExportOutput(self.camera.color_image_output_port(), "color_image")
+            # self.builder.ExportOutput(self.camera.depth_image_32F_output_port(), "depth_image")
+            # self.diagram = self.build()
+            # self.context = self.diagram.CreateDefaultContext()
+            rate.sleep()
+
+            # break
+
+
+    def plot_camera_images(self):
+        # system = self.build()
+        # Evaluate the camera output ports to get the images.
+        # context = system.CreateDefaultContext()
+        self.diagram.Publish(self.context)
+        color_image = self.diagram.GetOutputPort("color_image").Eval(self.context)
+        depth_image = self.diagram.GetOutputPort("depth_image").Eval(self.context)
+
+        # Plot the two images.
+        plt.subplot(121)
+        plt.imshow(color_image.data)
+        plt.title('Color image')
+        plt.subplot(122)
+        plt.imshow(np.squeeze(depth_image.data))
+        plt.title('Depth image')
+        #mpld3.display()
+        plt.show()
+
+    '''
+    Filter the simulated image from the real depth image
+    '''
+    def filter(self, real_img, sim_img):
+        return 
+
+
+if __name__ == "__main__":
+    # For testing
+    position = [0.03186507895588875, 0.03186507895588875, 1.1303866857968234, 0.3907924819735715, 0.170516990603166, -2.0551906674483877, 0.3345419575098929, 1.7596621570671762, 0.34174580770791413]
+    velocity = [0.0, 0.0, 0.011225922723739477, 0.07308963544653684, 0.6079061929818174, 0.26619868365598126, -0.013350198399852783, -0.7988742686585888, 0.03333959210447863]
+    meshcat = StartMeshcat()
+    # plot_camera_images(meshcat, position, velocity)
+    system = FrankaPlaybackSim(meshcat, position, velocity)
+    system.visualize()
