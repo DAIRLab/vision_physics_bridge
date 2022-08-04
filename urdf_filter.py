376c4d87138dbe3384346981e5f5d982f40e7cc1
ChrisSun99 <mengtisun@berkeley.edu>
2022-08-04 16:38:34 -0400

    TODO: Fix inaccurate mask

--- urdf_filter.py
+++ urdf_filter.py
@@ -2,36 +2,24 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pdb
+import imageio
+from PIL import Image
 from pydrake.multibody.parsing import Parser
-from pydrake.multibody.plant import MultibodyPlant, AddMultibodyPlantSceneGraph
+from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
-from pydrake.systems.rendering import MultibodyPositionToGeometryPose
-from pydrake.systems.primitives import TrajectorySource
 from pydrake.math import RigidTransform
-from pydrake.geometry import SceneGraph, DrakeVisualizer
-from pydrake.trajectories import PiecewisePolynomial
-from pydrake.all import (AbstractValue, AddMultibodyPlantSceneGraph, AngleAxis,
-                         BaseField, ConstantValueSource, CsdpSolver,
-                         DepthImageToPointCloud, DiagramBuilder,
-                         DifferentialInverseKinematicsIntegrator,
-                         DifferentialInverseKinematicsParameters, EventStatus, LeafSystem,
-                         MakePhongIllustrationProperties, MathematicalProgram,
-                         MeshcatPointCloudVisualizer, MeshcatVisualizer,
-                         MeshcatVisualizerParams, Parser, PiecewisePolynomial,
+from pydrake.all import (AddMultibodyPlantSceneGraph, AngleAxis,
+                         CameraInfo, ClippingRange, DepthRange,
+                         DepthRenderCamera, DiagramBuilder,
+                         MeshcatVisualizer, MeshcatVisualizerParams, Parser,
                          PiecewisePose, PointCloud, RigidTransform,
-                         RollPitchYaw, RotationMatrix, Simulator, StartMeshcat,
-                         ge)
+                         RollPitchYaw, RenderCameraCore, RotationMatrix, Simulator, StartMeshcat)
 from pydrake.common import FindResourceOrThrow
 from pydrake.common.eigen_geometry import AngleAxis
 from pydrake.multibody.meshcat import JointSliders
-from manipulation.meshcat_cpp_utils import (AddMeshcatTriad,
-                                            draw_open3d_point_cloud)
-from manipulation.mustard_depth_camera_example import MustardPointCloud
-from manipulation.open3d_utils import create_open3d_point_cloud
-from manipulation.scenarios import (AddMultibodyTriad, AddRgbdSensor,
-                                    MakeManipulationStation)
-from manipulation.utils import AddPackagePaths, FindResource
+from manipulation.scenarios import (AddMultibodyTriad, AddRgbdSensor)
+from manipulation.utils import FindResource
 import numpy as np
 import rospy
 from sensor_msgs.msg import JointState
@@ -61,11 +49,12 @@ class FrankaPlaybackSim:
 
         # Add a box for the camera in the environment.
         # World to camera frame transformation
+        camera_pos = [1.14164360, 0.15815239, 0.66422200]
         axis_vec = [-1.57165949, -1.63112887, 1.07928078]
         angle = np.linalg.norm(axis_vec)
         axis = axis_vec / angle
         angle_axis = AngleAxis(angle=angle,axis=axis)
-        self.X_Camera = RigidTransform(angle_axis, [1.14164360, 0.15815239, 0.66422200])
+        self.X_Camera = RigidTransform(angle_axis, camera_pos)
 
         self.camera_instance = self.parser.AddModelFromFile(FindResource("models/camera_box.sdf"))
         self.camera_frame = self.plant.GetFrameByName("base", self.camera_instance)    
@@ -79,7 +68,14 @@ class FrankaPlaybackSim:
             self.builder, self.scene_graph, self.meshcat, self.params
         )
         X_PC = RigidTransform()
-        self.camera = AddRgbdSensor(self.builder, self.scene_graph, X_PC=X_PC,
+        renderer = "my_renderer"
+        intrinsic = CameraInfo(640, 480, 380.2484436035156, 379.8265380859375, 314.2138977050781, 240.59800720214844)
+        depth_camera = DepthRenderCamera(
+            RenderCameraCore(
+                renderer, intrinsic,
+                ClippingRange(near=0.1, far=10.0), RigidTransform()),
+            DepthRange(0.1, 10.0))
+        self.camera = AddRgbdSensor(self.builder, self.scene_graph, X_PC=X_PC, depth_camera=depth_camera,
                            parent_frame_id=self.plant.GetBodyFrameIdOrThrow(
                                self.camera_frame.body().index()))
         self.camera.set_name("rgbd_sensor")
@@ -99,8 +95,6 @@ class FrankaPlaybackSim:
         self.simulator = Simulator(self.diagram, self.context)
         self.simulator.Initialize()
 
-        # TODO: why are the fingers so wide?
-        
         # ROS - temporarily don't need this since we are not processing in real-time
         # self.diagram = self.build()
         # self.simulator = Simulator(self.diagram)
@@ -134,45 +128,44 @@ class FrankaPlaybackSim:
         rate = rospy.Rate(4)
         rate.sleep()
         while not rospy.is_shutdown():
-            
             self.state = self.context.get_mutable_continuous_state_vector()
             self.state.SetFromVector(np.append(q,v))
             self.plot_camera_images()
             self.simulator.Initialize()
-            # self.builder.ExportOutput(self.camera.color_image_output_port(), "color_image")
-            # self.builder.ExportOutput(self.camera.depth_image_32F_output_port(), "depth_image")
-            # self.diagram = self.build()
-            # self.context = self.diagram.CreateDefaultContext()
             rate.sleep()
             # break
 
 
-    def plot_camera_images(self):
-        # system = self.build()
-        # Evaluate the camera output ports to get the images.
-        # context = system.CreateDefaultContext()
-        # self.diagram.Publish(self.context)
+    def plot_camera_images(self, frame_id):
         color_image = self.diagram.GetOutputPort("color_image").Eval(self.context)
         depth_image = self.diagram.GetOutputPort("depth_image").Eval(self.context)
 
         # Plot the two images.
         plt.subplot(121)
-        plt.imshow(color_image.data)
-        plt.title('Color image')
+        # plt.imshow(color_image.data)
+        # plt.title('Color image')
+        real = plt.imread('./data/frame00000{}.png'.format(frame_id))
+        real = real * 20
+        np.savetxt('./data/real_depth.txt', real)
+        _min = 0
+        _max = 1
+        plt.imshow(real, vmin = _min, vmax = _max)
+        plt.title('Real image')
         plt.subplot(122)
-        plt.imshow(np.squeeze(depth_image.data))
+        plt.imshow(np.squeeze(depth_image.data), vmin = _min, vmax = _max) #(480.640,1)
         plt.title('Depth image')
-        pdb.set_trace()
+        np.savetxt('./data/simulated_depth.txt', depth_image.data[:,:,0])
+    
+        # pdb.set_trace()
         #mpld3.display()
-        # plt.show()
-        cv2.imwrite('./data/simulated.png', depth_image.data)
+        plt.show()
     
 '''
 Filter the simulated image from the real depth image
 '''
-# def filter(real_img, sim_img):
-#     masked_img = np.subtract(real_img, sim_img)
-#     return masked_img
+def filter(real_img, sim_img):
+    masked_img = np.subtract(real_img, sim_img)
+    return masked_img
 
 def import_data():
     img_file = "./data/images.txt"
@@ -185,13 +178,19 @@ def import_data():
 
 if __name__ == "__main__":
     images, positions, velocities = import_data()
-    system = FrankaPlaybackSim(positions[0], velocities[0])
-    system.plot_camera_images()
-    simulated_img = cv2.imread('./data/simulated.png')
-    real_img = cv2.imread('./data/frame000000.png')
-    mask = cv2.subtract(real_img, simulated_img)
-    pdb.set_trace()
-    plt.figure()
-    plt.imshow(mask)
-    plt.show()
-    # cv2.imshow('mask', mask)
+    frame_id = 0
+    system = FrankaPlaybackSim(positions[frame_id], velocities[frame_id])
+    system.plot_camera_images(frame_id)
+    # simulated_image = np.loadtxt('./data/simulated_depth.txt')
+    # real_image = imageio.imread('./data/frame000009.png')
+    # real_image = real_image * 20
+    # mask = filter(real_image, simulated_image)
+    # im = Image.fromarray(mask)
+    # if im.mode != 'RGB':
+    #     im = im.convert('RGB')
+    # im.save("./data/mask.png")
+
+    # pdb.set_trace()
+    # plt.figure()
+    # plt.imshow(mask)
+    # plt.show()
