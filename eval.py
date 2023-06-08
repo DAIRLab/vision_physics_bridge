1b413d3913bc3f125f3da016467f36d5fcdbf8ed
ChrisSun99 <mengtisun@berkeley.edu>
2023-06-07 23:29:28 -0400

    Change to quat to fix gimbal lock

--- eval.py
+++ eval.py
@@ -37,7 +37,6 @@ def get_cosine_sim(frame_id):
     output_pose = np.loadtxt(OUTPUT_POSE_DIR + "%04i.txt" % frame_id)
     return 1 - sp.distance.cdist(gt_pose, output_pose, "cosine")
 
-
 def get_angle(P, Q):
     R = np.dot(P, Q.T)
     theta = (np.trace(R) - 1) / 2
@@ -52,7 +51,9 @@ def calculate_translation_error(estimated_pose, ground_truth_pose):
 def calculate_rotation_error(estimated_pose, ground_truth_pose):
     est_rotation = estimated_pose[:3, :3]
     gt_rotation = ground_truth_pose[:3, :3]
-    rotation_error = np.arccos((np.trace(np.dot(est_rotation.T, gt_rotation)) - 1) / 2)
+    cos_similarity = (np.trace(np.dot(est_rotation.T, gt_rotation)) - 1) / 2
+    cos_similarity = np.clip(cos_similarity, -1, 1)
+    rotation_error = np.arccos(cos_similarity)
     return rotation_error
 
 def calculate_success_rate(translation_errors, rotation_errors, translation_threshold, rotation_threshold):
@@ -61,6 +62,36 @@ def calculate_success_rate(translation_errors, rotation_errors, translation_thre
     success_rate = (success_count / num_frames) * 100
     return success_rate
 
+def matrix_to_quaternion(matrix):
+    q = np.empty((4,))
+    trace = matrix[0, 0] + matrix[1, 1] + matrix[2, 2]
+
+    if trace > 0:
+        s = 0.5 / np.sqrt(trace + 1.0)
+        q[0] = 0.25 / s
+        q[1] = (matrix[2, 1] - matrix[1, 2]) * s
+        q[2] = (matrix[0, 2] - matrix[2, 0]) * s
+        q[3] = (matrix[1, 0] - matrix[0, 1]) * s
+    else:
+        if matrix[0, 0] > matrix[1, 1] and matrix[0, 0] > matrix[2, 2]:
+            s = 2.0 * np.sqrt(1.0 + matrix[0, 0] - matrix[1, 1] - matrix[2, 2])
+            q[0] = (matrix[2, 1] - matrix[1, 2]) / s
+            q[1] = 0.25 * s
+            q[2] = (matrix[0, 1] + matrix[1, 0]) / s
+            q[3] = (matrix[0, 2] + matrix[2, 0]) / s
+        elif matrix[1, 1] > matrix[2, 2]:
+            s = 2.0 * np.sqrt(1.0 + matrix[1, 1] - matrix[0, 0] - matrix[2, 2])
+            q[0] = (matrix[0, 2] - matrix[2, 0]) / s
+            q[1] = (matrix[0, 1] + matrix[1, 0]) / s
+            q[2] = 0.25 * s
+            q[3] = (matrix[1, 2] + matrix[2, 1]) / s
+        else:
+            s = 2.0 * np.sqrt(1.0 + matrix[2, 2] - matrix[0, 0] - matrix[1, 1])
+            q[0] = (matrix[1, 0] - matrix[0, 1]) / s
+            q[1] = (matrix[0, 2] + matrix[2, 0]) / s
+            q[2] = (matrix[1, 2] + matrix[2, 1]) / s
+            q[3] = 0.25 * s
+    return q
 
 def plot_xyz(start_frame, end_frame):
     """Plot the x, y, z of BundleTrack output versus ground-truth poses of tagslam."""
@@ -159,8 +190,10 @@ def plot_with_time(bundletrack_time, gt_time, bundletrack_pose_dir, gt_pose_dir)
     bundletrack_time = np.array(bundletrack_time)
     output_x, output_y, output_z = [], [], []  # translation
     gt_x, gt_y, gt_z = [], [], []
-    output_alpha, output_beta, output_gamma = [], [], []
-    gt_alpha, gt_beta, gt_gamma = [], [], []
+    # output_alpha, output_beta, output_gamma = [], [], []
+    # gt_alpha, gt_beta, gt_gamma = [], [], []
+    estimated_w, estimated_x, estimated_y, estimated_z = [], [], [], []
+    ground_truth_w, ground_truth_x, ground_truth_y, ground_truth_z = [], [], [], []
     frame_num = len([name for name in os.listdir(bundletrack_pose_dir)])
     estimated_poses, ground_truth_poses = [], []
     for frame_id in range(1, frame_num + 1):
@@ -176,10 +209,15 @@ def plot_with_time(bundletrack_time, gt_time, bundletrack_pose_dir, gt_pose_dir)
         output_x.append(output_pose[0, 3])
         output_y.append(output_pose[1, 3])
         output_z.append(output_pose[2, 3])
-        alpha, beta, gamma = rotation_matrix_to_euler(output_pose[:3, :3])
-        output_alpha.append(alpha)
-        output_beta.append(beta)
-        output_gamma.append(gamma)
+        # alpha, beta, gamma = rotation_matrix_to_euler(output_pose[:3, :3])
+        # output_alpha.append(alpha)
+        # output_beta.append(beta)
+        # output_gamma.append(gamma)
+        w, x, y, z = matrix_to_quaternion(output_pose)
+        estimated_w.append(w)
+        estimated_x.append(x)
+        estimated_y.append(y)
+        estimated_z.append(z)
 
     for frame_id in range(1, len(gt_time) + 1):
         gt_frame = frame_id
@@ -188,10 +226,15 @@ def plot_with_time(bundletrack_time, gt_time, bundletrack_pose_dir, gt_pose_dir)
         gt_x.append(gt_pose[0, 3])
         gt_y.append(gt_pose[1, 3])
         gt_z.append(gt_pose[2, 3])
-        alpha, beta, gamma = rotation_matrix_to_euler(gt_pose[:3, :3])
-        gt_alpha.append(alpha)
-        gt_beta.append(beta)
-        gt_gamma.append(gamma)
+        # alpha, beta, gamma = rotation_matrix_to_euler(gt_pose[:3, :3])
+        # gt_alpha.append(alpha)
+        # gt_beta.append(beta)
+        # gt_gamma.append(gamma)
+        w_, x_, y_, z_ = matrix_to_quaternion(gt_pose)
+        ground_truth_w.append(w_) 
+        ground_truth_x.append(x_)
+        ground_truth_y.append(y_)
+        ground_truth_z.append(z_)
 
     output_x, output_y, output_z = (
         np.array(output_x),
@@ -214,7 +257,7 @@ def plot_with_time(bundletrack_time, gt_time, bundletrack_pose_dir, gt_pose_dir)
     print(f"Rotation Error: {np.degrees(np.mean(rotation_errors)):.4f} degrees")
     print(f"Success Rate: {success_rate:.2f}%")
 
-    fig, axs = plt.subplots(2, 3)
+    fig, axs = plt.subplots(2, 4)
     # fig.suptitle("Toss %i" % TOSS_IDX)
     axs[0, 0].plot(bundletrack_time, output_x, label="BundleTrack")
     axs[0, 0].plot(gt_time, gt_x, label="ground-truth")
@@ -224,26 +267,41 @@ def plot_with_time(bundletrack_time, gt_time, bundletrack_pose_dir, gt_pose_dir)
     axs[0, 1].plot(bundletrack_time, output_y, label="BundleTrack")
     axs[0, 1].plot(gt_time, gt_y, label="ground-truth")
     axs[0, 1].set_title("Position y")
-    axs[0, 1].legend()
 
     axs[0, 2].plot(bundletrack_time, output_z, label="BundleTrack")
     axs[0, 2].plot(gt_time, gt_z, label="ground-truth")
     axs[0, 2].set_title("Position z")
-    axs[0, 2].legend()
+    
+    axs[1, 0].plot(bundletrack_time, estimated_w, label="BundleTrack")
+    axs[1, 0].plot(gt_time, ground_truth_w, label="ground-truth")
+    axs[1, 0].set_title("w")
+    
+    axs[1, 1].plot(bundletrack_time, estimated_x, label="BundleTrack")
+    axs[1, 1].plot(gt_time, ground_truth_x, label="ground-truth")
+    axs[1, 1].set_title("x")
+    
+    axs[1, 2].plot(bundletrack_time, estimated_y, label="BundleTrack")
+    axs[1, 2].plot(gt_time, ground_truth_y, label="ground-truth")
+    axs[1, 2].set_title("y")
+    
+    axs[1, 3].plot(bundletrack_time, estimated_z, label="BundleTrack")
+    axs[1, 3].plot(gt_time, ground_truth_z, label="ground-truth")
+    axs[1, 3].set_title("z")
 
-    axs[1, 0].plot(bundletrack_time, output_alpha, label="BundleTrack")
-    axs[1, 0].plot(gt_time, gt_alpha, label="ground-truth")
-    axs[1, 0].set_title("Roll")
+    # axs[1, 0].plot(bundletrack_time, output_alpha, label="BundleTrack")
+    # axs[1, 0].plot(gt_time, gt_alpha, label="ground-truth")
+    # axs[1, 0].set_title("Roll")
 
-    axs[1, 1].plot(bundletrack_time, output_beta, label="BundleTrack")
-    axs[1, 1].plot(gt_time, gt_beta, label="ground-truth")
-    axs[1, 1].set_title("Pitch")
+    # axs[1, 1].plot(bundletrack_time, output_beta, label="BundleTrack")
+    # axs[1, 1].plot(gt_time, gt_beta, label="ground-truth")
+    # axs[1, 1].set_title("Pitch")
 
-    axs[1, 2].plot(bundletrack_time, output_gamma, label="BundleTrack")
-    axs[1, 2].plot(gt_time, gt_gamma, label="ground-truth")
-    axs[1, 2].set_title("Yaw")
+    # axs[1, 2].plot(bundletrack_time, output_gamma, label="BundleTrack")
+    # axs[1, 2].plot(gt_time, gt_gamma, label="ground-truth")
+    # axs[1, 2].set_title("Yaw")
 
     spacing = 0.100
+    fig.subplots_adjust(hspace=0.5)
     fig.subplots_adjust(bottom=spacing)
     plt.show()
     plt.savefig('result_bundletrack2.0_latest.png')
