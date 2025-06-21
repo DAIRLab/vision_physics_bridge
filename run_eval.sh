ecb57dba28f493737f2c316cecbeb0dd3a41919d
Minghan Zhu <minghanz@umich.edu>
2025-06-20 20:04:31 -0400

    Relocation of obselete files

--- run_eval.sh
+++ run_eval.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 
-python eval_cube_old.py 1 1 --no_show
-python eval_cube_old.py 1 2 --no_show
-python eval_cube_old.py 2 1 --no_show
-python eval_cube_old.py 2 2 --no_show
-
-python eval_cube_old.py 3 1 --no_show
-python eval_cube_old.py 3 2 --no_show
-python eval_cube_old.py 4 1 --no_show
-python eval_cube_old.py 4 2 --no_show
-
-python eval_cube_old.py 5 1 --no_show
-python eval_cube_old.py 5 2 --no_show
-python eval_cube_old.py 6 1 --no_show
-python eval_cube_old.py 6 2 --no_show
-
-python eval_cube_old.py 7 1 --no_show
-python eval_cube_old.py 7 2 --no_show
-python eval_cube_old.py 8 1 --no_show
-python eval_cube_old.py 8 2 --no_show
-
-python eval_cube_old.py 9 1 --no_show
-python eval_cube_old.py 9 2 --no_show
-python eval_cube_old.py 10 1 --no_show
-python eval_cube_old.py 10 2 --no_show
+asset=bakingbox_1
+python mesh_processing.py manual_icp --vision-asset=robotocc_${asset} --bundlesdf-id=00-onescript --nerf-bundlesdf-id=00-onescript --cycle-iteration=1 --meshlab
+python evaluate.py --vision-asset=robotocc_${asset} --bundlesdf-id=00-onescript --nerf-bundlesdf-id=00-onescript --cycle-iteration=1 #--do-videos
